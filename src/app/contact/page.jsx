1c8f40513847af2dd4c8e9ce99ec12c53e1a8745
annaLucian <garielapoves01@gmail.com>
2023-12-22 16:07:12 +0100

    feat: :rocket: add metadata to each page, add jest dependencie

--- src/app/contact/page.jsx
+++ src/app/contact/page.jsx
@@ -1,3 +1,8 @@
+export const metadata = {
+    title: 'Contact',
+    description: 'This page shows a contact form.',
+  }
+
 export default function Contact(){
     return(
         <h1>Contact</h1>
---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,5 @@
+export default function Header(){
+    return(
+        <p>Hola soy un header</p>
+    )
+}
