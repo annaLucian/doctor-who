c652d31e2dfd0812069090cf3c1cd514cbb6702e
annaLucian <garielapoves01@gmail.com>
2023-12-22 22:24:28 +0100

    feat: :lipstick: create the footer component, end of css styles

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,19 +1,22 @@
+'use client';
 import GlobalStyle from './global.styles';
 import Header from '@/components/Header';
+import Footer from '@/components/Footer';
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <head>
-        <link
+        {/* <link
           rel="stylesheet"
           href="'https://fonts.googleapis.com/css2?family=Lato:wght@700&family=Montserrat:ital,wght@0,400;0,600;1,400&display=swap'"
-        />
+        /> */}
       </head>
       <body>
         <GlobalStyle />
         <Header />
         {children}
+        <Footer />
       </body>
     </html>
   );
---
+++ src/components/Footer/footer.styles.jsx
@@ -0,0 +1,24 @@
+import styled from 'styled-components';
+
+export const StyledFooter = styled.footer`
+  display: flex;
+  padding: 2rem;
+  border-top: 0.1rem solid white;
+  justify-content: space-between;
+  .social-links {
+    display: flex;
+    gap: 1.2rem;
+  }
+  @media (min-width: 1000px) {
+    padding: 1rem 6rem;
+    .social-links li svg path {
+      transition: all ease 0.8s;
+    }
+    .social-links li:hover svg path {
+      fill: #ffc67e;
+    }
+  }
+  @media (min-width: 1600px) {
+    padding: 1rem 8rem;
+  }
+`;
