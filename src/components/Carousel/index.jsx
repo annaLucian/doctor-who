7ab631a71d0595ea9a26ed13ca8ac7afc5a96618
annaLucian <garielapoves01@gmail.com>
2023-12-23 00:02:00 +0100

    feat: :rocket: installation of react slick for carousel use

---
+++ src/components/Carousel/index.jsx
@@ -0,0 +1,43 @@
+'use client';
+import 'slick-carousel/slick/slick.css';
+import 'slick-carousel/slick/slick-theme.css';
+import Slider from 'react-slick';
+
+export default function Carousel() {
+  const settings = {
+    dots: true,
+    infinite: true,
+    arrows: false,
+    autoplay: true,
+    fade: true,
+    speed: 1000,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    adaptiveHeight: true,
+  };
+  return (
+    <div>
+      <h2> Single Item</h2>
+      <Slider {...settings}>
+        <div>
+          <h3>1</h3>
+        </div>
+        <div>
+          <h3>2</h3>
+        </div>
+        <div>
+          <h3>3</h3>
+        </div>
+        <div>
+          <h3>4</h3>
+        </div>
+        <div>
+          <h3>5</h3>
+        </div>
+        <div>
+          <h3>6</h3>
+        </div>
+      </Slider>
+    </div>
+  );
+}
