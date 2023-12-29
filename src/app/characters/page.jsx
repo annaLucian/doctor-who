79038700642c05a2adc36bfe71958e0bea48522a
annaLucian <garielapoves01@gmail.com>
2023-12-29 01:29:21 +0100

    feat: :sparkles: made the styles of the page characters with media queries.

--- src/app/characters/page.jsx
+++ src/app/characters/page.jsx
@@ -1,7 +1,36 @@
-export const metadata = {
-  title: 'Characters',
-  description: 'This page shows information about a character.',
-};
+'use client';
+import Image from 'next/image';
+import { StyledButton } from '@/components/Carousel/carousel.styles';
+import { StyledCharacter } from '@/app/characters/characters.styles';
+// export const metadata = {
+//   title: 'Characters',
+//   description: 'This page shows information about a character.',
+// };
 export default function Characters() {
-  return <h1>characters</h1>;
+  return (
+    <StyledCharacter>
+      <h1>Conoce a ... decimotercera doctor</h1>
+      <div className="character__img">
+        <Image
+          src="/assets/img/doctors/Thirteenth_Doctor_Jodie_Whittaker.jpg"
+          width={260}
+          height={390}
+          alt="decimotercera doctora de la serie doctcharacter__imgor who"
+          className=""
+        />
+      </div>
+      <div className="character__content">
+        <h1>Conoce a ... decimotercera doctor</h1>
+        <p>
+          La Decimotercera Doctora es una brillante bola de energía,
+          hiperactiva, excitable y que habla a mil por hora desde el primer
+          momento en que la conocemos. Llena de asombro y admiración por el
+          universo, es una exploradora encantadora que defiende la justicia y la
+          bondad siempre que puede. En sus propias palabras: Cuando la gente
+          necesita ayuda, nunca me niego.
+        </p>
+        <StyledButton>Ver más</StyledButton>
+      </div>
+    </StyledCharacter>
+  );
 }
