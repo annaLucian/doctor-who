f5cb72ca35ee78b5920988c82bf615e38dd385dc
annaLucian <garielapoves01@gmail.com>
2023-12-30 00:34:36 +0100

    feat: :rocket: create error page, and section it when a search is not found

---
+++ src/app/not-found.jsx
@@ -0,0 +1,19 @@
+import styles from './error.module.css';
+import Link from 'next/link';
+
+export default function NotFound() {
+  return (
+    <section className={styles.errorContainer}>
+      <div className={styles.errorContent}>
+        <h2>404 | página no encontrada</h2>
+        <h6>Oh, madre mía. creo que seria mejor regresar a la TARDIS!!</h6>
+        <p>
+          Puedes seguir explorando más contenido en nuestra página
+          <div className={styles.link}>
+            <Link href="/">Home</Link>
+          </div>
+        </p>
+      </div>
+    </section>
+  );
+}
