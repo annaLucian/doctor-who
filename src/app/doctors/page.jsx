73427473e1d4a80f357ee7f4ed783eb42c27fad0
annaLucian <garielapoves01@gmail.com>
2023-12-29 15:37:25 +0100

    feat: :rocket: api implementation finishing

--- src/app/doctors/page.jsx
+++ src/app/doctors/page.jsx
@@ -1,25 +1,21 @@
-'use client';
 import Card from '@/components/Card';
 import styles from '@/app/doctors/styles.module.css';
-// export const metadata = {
-//   title: 'Doctors',
-//   description: 'This page shows a list of doctors.',
-// };
+import { getData } from '@/services/doctor';
+export const metadata = {
+  title: 'Doctors | Doctor who',
+  description: 'This page shows a list of doctors.',
+};
 
-export default function Doctors() {
+export default async function Doctors() {
+  const doctorsData = await getData();
   return (
     <section className={styles.doctors__section}>
       <h1 className={styles.doctors__Title}>Doctores</h1>
-      <di className={styles.cards__container}>
-        <Card />
-        <Card />
-        <Card />
-        <Card />
-        <Card />
-        <Card />
-        <Card />
-        <Card />
-      </di>
+      <div className={styles.cards__container}>
+        {doctorsData.map((el) => (
+          <Card key={el.id} doctor={el} />
+        ))}
+      </div>
     </section>
   );
 }
--- src/components/Card/index.jsx
+++ src/components/Card/index.jsx
@@ -1,9 +1,10 @@
+'use client';
 import { StyledCard } from '@/components/Card/card.styles';
 import Image from 'next/image';
 import { StyledButton } from '@/components/Carousel/carousel.styles';
 import { useState } from 'react';
 
-export default function Card() {
+export default function Card({ doctor }) {
   const [isHover, setIsHover] = useState(false);
   const handleMouseOver = () => {
     setIsHover(true);
@@ -13,16 +14,16 @@ export default function Card() {
     setIsHover(false);
   };
   return (
-    <StyledCard $ishover={isHover}>
+    <StyledCard $ishover={isHover} $urlImg={doctor.image}>
       <div className="card__background">
         <div className={`${isHover ? 'animate-bg' : ''}`}></div>
         <div className="card__content">
           <div className="bg__image"></div>
         </div>
       </div>
-      <h5>Primer Doctor</h5>
-      <p>Peter Cushing</p>
-      <time>1965-1966</time>
+      <h5>{doctor.doctor}</h5>
+      <p>{doctor.actor}</p>
+      <time>{doctor.screen_time}</time>
       <StyledButton
         onMouseLeave={handleOnMouseLeave}
         onMouseOver={handleMouseOver}
